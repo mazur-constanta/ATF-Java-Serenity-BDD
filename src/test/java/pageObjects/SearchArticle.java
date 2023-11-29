deb9bce1c539fd42b9393b02b221b0dc13746568
Constanta <constancegerman@gmail.com>
2023-11-29 18:38:35 +0200

    added basic severity BDD code

---
+++ src/test/java/pageObjects/SearchArticle.java
@@ -0,0 +1,7 @@
+package pageObjects;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class SearchArticle {
+    public static final Target BODY =  Target.the("article identifier").locatedBy("//article");
+}
